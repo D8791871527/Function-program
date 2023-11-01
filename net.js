b64d126246a9ea4551e6a06966b2154b723750d9
student Deepak <Dkumar20208958805345@gmail.com>
2023-11-01 11:54:54 -0700

    web project

---
+++ net.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".content", {
+    strings: ["Frontend Developer", "Youtuber", "Web Developer" ,""],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
+var f=document.querySelector("h3");
+
+f.innerHTML="Thi is college student";
+f.style.color="red"
+f.style.backgroundColor="yellow"
+
+var r=document.getElementById("rn")
+r.style.height="40%"
